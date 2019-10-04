2b7539f6e59ffe9f45e089056fdd8459de5e531e
Admin <naveenkaushal441@gmail.com>
2019-10-04 20:15:51 +0530

    Main added

---
+++ HoursEntry/Main.java
@@ -0,0 +1,30 @@
+package HoursEntry;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application 
+{
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			Parent root=FXMLLoader.load(getClass().getResource("HoursView.fxml")); 
+			Scene scene = new Scene(root,560,674);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
