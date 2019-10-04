aef35f7172ee5022e76d28ce71939082acb90744
Admin <naveenkaushal441@gmail.com>
2019-10-04 21:58:45 +0530

    Connection added

---
+++ HoursEntry/My2DbConnection.java
@@ -0,0 +1,33 @@
+package HoursEntry;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+public class My2DbConnection
+{
+	public static Connection doConnectCar()
+	{
+		Connection con=null; //LRV
+		try {
+			
+			 Class.forName("com.mysql.jdbc.Driver");
+			 //														    databaseName      password
+			 con=	DriverManager.getConnection("jdbc:mysql://localhost/info","root","Nav1een.p");
+			 System.out.println("Connected"); 
+		 
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public static void main(String[] args) 
+	{
+		doConnectCar();
+
+	}
+
+}
